35cf6989b04c3e5406c700595e816d18b7569d47
Priyanshu Sharma <100065096+Priyanshu2sh@users.noreply.github.com>
2025-03-04 13:13:32 +0530

    installed pymsql

--- energy_transition/settings.py
+++ energy_transition/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Load environment variables from .env file
 load_dotenv()
