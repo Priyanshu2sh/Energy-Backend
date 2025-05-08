d6a410578a332a161a0ed27b7d8077d4dc7460b2
Priyanshu Sharma <100065096+Priyanshu2sh@users.noreply.github.com>
2025-05-08 15:51:33 +0530

    added model response logs

--- energy/views.py
+++ energy/views.py
@@ -1349,6 +1349,7 @@ class OptimizeCapacityAPI(APIView):
                     logger.debug(re_replacement)
                     logger.debug(input_data)
                     response_data = optimization_model(input_data, hourly_demand=numeric_hourly_demand, re_replacement=re_replacement, valid_combinations=valid_combinations, OA_cost=(ISTS_charges + master_record.state_charges)*1000)
+                    logger.debug(response_data)
 
                     if response_data != 'The demand cannot be met by the IPPs':
                         for combination_key, details in response_data.items():
